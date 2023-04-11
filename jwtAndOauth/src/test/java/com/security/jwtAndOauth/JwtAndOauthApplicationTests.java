da3b3bc3c140996cc602fa633971d59eed342473
Hosung <an3735297@pusan.ac.kr>
2023-04-11 20:24:10 +0900

    feat : create project
    project를 생성하고 연결을 test 하였습니다.

---
+++ jwtAndOauth/src/test/java/com/security/jwtAndOauth/JwtAndOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security.jwtAndOauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtAndOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
