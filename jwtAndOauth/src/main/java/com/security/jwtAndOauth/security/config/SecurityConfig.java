e0fc78ac65813400bdf12d6411070e4e7ef924af
Hosung <an3735297@pusan.ac.kr>
2023-04-11 23:34:48 +0900

    feat : controller, securityConfig
    sample controller 와 개발할 securityConfig file 을 생성하였습니다.

---
+++ jwtAndOauth/src/main/java/com/security/jwtAndOauth/security/config/SecurityConfig.java
@@ -0,0 +1,17 @@
+package com.security.jwtAndOauth.security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests().anyRequest().authenticated();
+        http.formLogin();
+        http.httpBasic();
+        return http.build();
+    }
+}
