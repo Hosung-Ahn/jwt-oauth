c4a9fcdd4c82fbc1aa51b3378f4706ddc7e48ce9
Hosung <an3735297@pusan.ac.kr>
2023-04-15 21:47:08 +0900

    fix : redis error about timeout

--- jwt/src/main/java/com/example/jwt/security/refreshtoken/RefreshTokenRepository.java
+++ jwt/src/main/java/com/example/jwt/security/refreshtoken/RefreshTokenRepository.java
@@ -5,13 +5,15 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Repository;
 
+import java.util.concurrent.TimeUnit;
+
 @RequiredArgsConstructor
 @Repository
 public class RefreshTokenRepository {
     private final RedisTemplate<String, String> redisTemplate;
 
     public void setWithTimeout(String key, String value, long timeout) {
-        redisTemplate.opsForValue().set(key, value, timeout);
+        redisTemplate.opsForValue().set(key, value, timeout, TimeUnit.SECONDS);
     }
 
     public String get(String key) {
